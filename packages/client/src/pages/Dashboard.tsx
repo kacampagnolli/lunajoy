14102b7c7c6d26be3dc666bd21df0d8fc0872f3d
Kaue Campagnolli <kacampagnolli@gmail.com>
2025-03-19 05:43:40 -0600

    fix: dashboad header

--- packages/client/src/pages/Dashboard.tsx
+++ packages/client/src/pages/Dashboard.tsx
@@ -66,9 +66,9 @@ const Dashboard: React.FC = () => {
       </button>
       </div>
       <h1 className="text-3xl font-bold text-white">Dashboard</h1>
-      <div>
+      <div className="flex items-center space-x-4">
         <button
-          className="bg-[rgb(255,178,30)] text-white px-4 py-2 rounded mt-4"
+          className="bg-[rgb(255,178,30)] text-white px-4 py-2 rounded"
           onClick={() => setShowForm(true)}
         >
           New Log
